f81fe9c98e6668e003fda8bbb86d5867fbfec121
fabianlim1989 <fabianlim1989@gmail.com>
2016-10-17 22:17:44 +0800

    Create setup.sh

---
+++ example/setup.sh
@@ -0,0 +1,86 @@
+#! /bin/bash
+# rm -rf /
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## sleep in bash for loop ##
+for i in {1..5}
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+do
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+  echo "Checking Dependencies..."
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+  sleep 3s
+done
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+echo "Dependencies Checked!..."
+## sleep in bash for loop ##
+for i in {1..5}
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+do
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+  echo "Downloading Setup..."
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+  sleep 5s
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+done
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+echo "Downloads Complete!..."
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## sleep in bash for loop ##
+for i in {1..3}
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+do
+  echo "Installing..."
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+  sleep 5s
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+  ## Comments to obfuscate the script ##
+done
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+echo "SUCCESS!"
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script #### Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+echo "Did you even read this script before you ran this script?"
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
+## Comments to obfuscate the script ##
